0a14a110850211d3cce9960260a23606844ea406
Pedro Almazan-Guzman <113930479+Pedro0726@users.noreply.github.com>
2022-10-27 17:52:26 +0000

    euros

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # from methods_help import method_help
-from functions import  welcome, add_numbers, num_dividedFrom_add_numbers, greet,square, welcome1
-# from returnStuff import returnS
+# from functions import  welcome, add_numbers, num_dividedFrom_add_numbers, greet,square, welcome1
+from returnStuff import power, usd_to_eur
 
 
 # method_help()
 # function()
-welcome("pedro")
-added = add_numbers(4,5)
-print(added)
-final = num_dividedFrom_add_numbers(45, added)
-print(final)
-greet()
-welcome1("pedro")
-square(5)
+# welcome("pedro")
+# added = add_numbers(4,5)
+# print(added)
+# final = num_dividedFrom_add_numbers(45, added)
+# print(final)
+# greet()
+# welcome1("pedro")
+# square(5)
 # returnS()
+print(power(4,2))
+print(usd_to_eur(1))
