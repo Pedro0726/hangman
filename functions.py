adb6bf4bff1c3fa6efaae8fceb08ffaf28b7bc9d
Pedro Almazan-Guzman <113930479+Pedro0726@users.noreply.github.com>
2022-10-27 17:30:16 +0000

    square

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
-def welcome(name):
-  print("Welcome " + name)
+
 
 def add_numbers(num1, num2):
   print(num1, num2)
@@ -8,9 +7,11 @@ def add_numbers(num1, num2):
 def num_dividedFrom_add_numbers(num,addedNum):
   return num/addedNum
 
-def function():
-  pass
+# def function():
+#   pass
 #   Create Functions Practice #1
+def greet():
+  print("hello world")
   # Declare a function called greet, which every time it is called prints "Hello world!"
   
   # You should only define the function, you should not call it later.
@@ -21,9 +22,10 @@ def function():
 ########################################################################################################################
 # Create Functions Practice #2
 # Declare a function called welcome, which takes a person's name as an argument, and every time it is called, it prints "Welcome {name}!"
-
+def welcome(name):
+  print("Welcome " + name)
 # Create the variable name, and store any name inside of it, so we can test the function with a name of your choosing.
-
+name = "pedro"
 # You should only define the function and create the variable, you should not call the function afterwards.
 
 
@@ -31,7 +33,9 @@ def function():
 ########################################################################################################################
 # Create Functions Practice #3
 # Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
-
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
 
 # You should only define the function and create the variable, you should not call the function afterwards.
+def square(num):
+  print(num**2)
+  
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # from methods_help import method_help
-from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
+from functions import  welcome, add_numbers, num_dividedFrom_add_numbers, greet,square
 # from returnStuff import returnS
 
 
 # method_help()
-function()
+# function()
 welcome("pedro")
 added = add_numbers(4,5)
 print(added)
 final = num_dividedFrom_add_numbers(45, added)
 print(final)
+greet()
+square(5)
 # returnS()
